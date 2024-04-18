ff78c349e621d27e0884788bcb05ce43a5f5d240
Ghaliahsalman95 <159685763+Ghaliahsalman95@users.noreply.github.com>
2024-04-18 14:51:50 +0300

    Delete testcapstone3 directory

--- testcapstone3/ApiResponse/APIResponse.java
+++
@@ -1,9 +0,0 @@
-package com.example.testcapstone3.ApiResponse;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data@AllArgsConstructor
-public class APIResponse {
-    private String response;
-}
