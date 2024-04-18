fc7257aa7a78845794a6fce67bb1ec8f9f363a55
Ghaliahsalman95 <159685763+Ghaliahsalman95@users.noreply.github.com>
2024-04-18 14:47:27 +0300

    Delete capstone2 directory

--- capstone2/ApiResponse/APIResponse.java
+++
@@ -1,9 +0,0 @@
-package com.example.capstone2.ApiResponse;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data@AllArgsConstructor
-public class APIResponse {
-    private String response;
-}
